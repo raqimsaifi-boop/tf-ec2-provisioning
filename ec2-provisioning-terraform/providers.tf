f50c871c91b0e889492f82152782ffd2ba60f33a
raqimsaifi-boop <raqimsaifi@gmail.com>
2026-01-14 18:05:43 +0530

    .

--- ec2-provisioning-terraform/providers.tf
+++ ec2-provisioning-terraform/providers.tf
@@ -1,3 +1,17 @@
+# provider "aws" {
+#   region = var.region
+# }
+
+
+
+# providers.tf
 provider "aws" {
   region = var.region
+
+  # assume into target account for all resource operations
+  assume_role {
+    role_arn     = var.target_role_arn
+    session_name = "tf-session"
+    # external_id  = var.external_id  # if your trust requires it
+  }
 }
